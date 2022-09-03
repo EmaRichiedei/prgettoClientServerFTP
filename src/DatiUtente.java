ea6ed0cb3e53dd7815c0ae2d47dfd5613f6ccc83
EmanueleRichiedei <emanuele.richiedei@ieee.org>
2022-09-03 22:34:42 +0200

    aggiunta modellizzazione dei dati utente

---
+++ src/DatiUtente.java
@@ -0,0 +1,22 @@
+public class DatiUtente {
+    private String nomeUtente=null;
+    private String password=null;
+    public DatiUtente(){
+
+    }
+    public DatiUtente(String _nomeUtente,String _password){
+        nomeUtente=_nomeUtente;
+        password=_password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+    public void salvaUtenti(){
+
+    }
+}
