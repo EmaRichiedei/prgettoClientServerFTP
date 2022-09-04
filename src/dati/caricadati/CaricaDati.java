b42ad2cd93f2058bd888263175bb3e005ee58941
EmanueleRichiedei <emanuele.richiedei@ieee.org>
2022-09-04 21:35:51 +0200

    aggiunta IOeccezione

--- src/dati/caricadati/CaricaDati.java
+++ src/dati/caricadati/CaricaDati.java
@@ -1,14 +1,16 @@
 package dati.caricadati;
 
+import java.io.IOException;
+
 public interface CaricaDati {
     /*
      *   Acquisizione di dati da sorgente esterna
      *
      */
 
-    public String leggi(String sorgente) ;  // acquisisce i dati dalla sorgente
+    public String leggi(String sorgente) throws IOException  ;// acquisisce i dati dalla sorgente
     public boolean infoNonPresente(String sorgente);    // controllo incrociato tra esiestenza della sorgente e della presenza di dati sulla sorgente
-    public boolean isSorgentePresente(String sorgente);  //controlla se esiste la sorgente dei dati da caricare
+    public boolean isSorgenteEsistente(String sorgente);  //controlla se esiste la sorgente dei dati da caricare
     public boolean isSorgenteVuota( String sorgente);  //controlla se la sorgente ha dati
 
 
