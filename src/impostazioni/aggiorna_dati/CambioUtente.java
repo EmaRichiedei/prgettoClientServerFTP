0729f0e2411354eea468284f3b795aa99c8646d7
EmanueleRichiedei <emanuele.richiedei@ieee.org>
2022-09-04 21:45:48 +0200

    rimosso caricamento file utente

--- src/impostazioni/aggiorna_dati/CambioUtente.java
+++ src/impostazioni/aggiorna_dati/CambioUtente.java
@@ -1,11 +1,7 @@
 package impostazioni.aggiorna_dati;
 
 import core.Configurazione;
-import salvadati.ScriviFile;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import dati.salvadati.ScriviFile;
 
 public class CambioUtente {
 
@@ -23,40 +19,6 @@ public class CambioUtente {
         scrivi.scrivi(token, Configurazione.FILE_CONFIGURAZIONE_UTENTE);
     }
 
-    /***
-     * importo i dati dell'utente dal file "utente.txt" e li restituisco in un unica srtringa nel formato
-     *
-     *     nomeUtente|password
-     *
-     * @param nomeFile
-     * @return
-     * @throws IOException
-     */
-    public String leggiDatiUtente(String nomeFile) throws  IOException{
-
-        String token=null;
-
-        BufferedReader reader = new BufferedReader(new FileReader(nomeFile));
-        String line ;
-
-        token = reader.readLine();
-        reader.close();
-
-        return token;
-    }
-
-    public String getPassword() throws IOException {
-        String token = leggiDatiUtente(Configurazione.FILE_CONFIGURAZIONE_UTENTE);
-        String password = token.substring(token.indexOf("|")+1)  ;
-        return password;
-    }
-
-    public String getNomeUtente() throws  IOException{
-        String token = leggiDatiUtente(Configurazione.FILE_CONFIGURAZIONE_UTENTE);
-        String nomeUtente = token.substring(0,token.indexOf("|"))  ;
-        return nomeUtente;
-
-    }
 
 
 }
